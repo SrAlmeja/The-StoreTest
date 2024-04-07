3050f273ec2eec46774dc2c373f03c93e0cc2b75
Angel Legorreta <cdmx1895@amerike.edu.mx>
2024-04-07 17:25:35 -0500

    Main Menu

---
+++ The Dungeon Store/Assets/8BIT CanariPack TopDown/Almeja Studo/Code/Managers/SceneController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    #region Variables
+
+    private static SceneController _instance;
+    public static  SceneController Instance
+    {
+        get { return _instance; }
+    }
+
+    #endregion
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+            Debug.LogError("Mas controladores de tipo Scenetransition se han encontrado en la escena");
+            return;
+        }
+
+        _instance = this;
+        
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void LoadScene(string sceneToLoad)
+    {
+        SceneManager.LoadSceneAsync(sceneToLoad);
+    }
+    
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+    
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+    }
+    
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
